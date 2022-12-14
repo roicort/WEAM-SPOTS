e0e6ee6397b463801513fdb2aef500afd277774a
Rafael Morales Gamboa <r.moralesgamboa@gmail.com>
2022-12-14 11:02:05 -0600

    Adjustments to code for analysing classification.

--- classif_dreams.py
+++ classif_dreams.py
@@ -7,24 +7,26 @@ fname = constants.csv_filename(
     constants.chosen_prefix,es)
 chosen = np.genfromtxt(fname, dtype=int, delimiter=',')
 msize = 4
+# sigmas = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50]
+sigma_index = 2
+n_depths = 6
 
 for fold in range(constants.n_folds):
     label = chosen[fold,0]
     idx = chosen[fold,1]
-    # Uncomment for regular input
+    # Uncomment for regular dreaming
     # prefix = constants.classification_name(es)
-    # Uncomment for noised input
+    # Uncomment for noised dreaming
     prefix = constants.noised_classification_name(es)
     fname = constants.data_filename(prefix, es, fold)
     classif = np.load(fname)
     print(f'{label}, {classif[idx]}', end='')
-    sigmas = [0.05, 0.10, 0.20, 0.30, 0.40, 0.50]
+    # Uncomment for noised dreaming
+    prefix = constants.classification_name(es) + constants.noised_suffix
     prefix += constants.int_suffix(msize, 'msz')
-    for s in sigmas:
-        suffix = constants.float_suffix(s, 'sgm')
-        fname = prefix + suffix
-        fname = constants.data_filename(fname, es, fold)
-        classif = np.load(fname)
-        print(f', {classif[idx]}', end = '')
-    print('')
+    fname = constants.csv_filename(prefix, es, fold)
+    classif = np.genfromtxt(fname, delimiter=',')
+    start = sigma_index*6
+    classif = classif[start:start+n_depths]
+    print(classif)
 
