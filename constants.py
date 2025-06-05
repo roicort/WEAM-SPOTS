875516fea9adff135317468d0470ca40b1da004e
zǝʇɹoɔ oɓıɹpoɹ <28023409+roicort@users.noreply.github.com>
2025-06-04 22:30:41 -0600

    Exp

--- constants.py
+++ constants.py
@@ -84,7 +84,7 @@ learning_suffixes = [[original_suffix], [agreed_suffix], [amsystem_suffix],
 
 # Number of columns in memory
 domain = 256
-n_folds = 5
+n_folds = 1
 n_jobs = -1
 dreaming_cycles = 6
 
